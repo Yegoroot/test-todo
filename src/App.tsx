91da253dc7f1f32a4647173453e5f4c1348e50d0
Yegor Kirsanov <easywayroot@gmail.com>
2024-08-27 12:55:20 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Check } from '@mui/icons-material'
+import { ToggleButton } from '@mui/material'
+import { useState } from 'react'
 
 function App() {
+  const [selected, setSelected] = useState(false)
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToggleButton
+        value="check"
+        selected={selected}
+        onChange={() => {
+          setSelected(!selected)
+        }}
+      >
+        <Check />
+      </ToggleButton>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
